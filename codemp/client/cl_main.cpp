1ab293e3c1a88fdb2dca532d9525681905ec04c6
deathsythe47 <deathsythe47@gmail.com>
2018-09-13 19:21:53 -0700

    don't care about this nerd shit just enjoy video games

--- codemp/client/cl_main.cpp
+++ codemp/client/cl_main.cpp
@@ -621,7 +621,7 @@ void CL_DemoRestart_f(void) {
 	}
 
 	Com_Printf("Restarting demo \"%s\".", demoname);
-	Cbuf_ExecuteText(EXEC_NOW, va("demo \"%s\"\n", demoname));
+	Cbuf_ExecuteText(EXEC_APPEND, va("demo \"%s\"\n", demoname));
 }
 
 void CL_DelDemo_f(void) {
