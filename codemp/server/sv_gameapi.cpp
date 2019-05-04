e398ee98c198c8b5313d0b95cb32e10b7f03fc62
Bucky21659 <mjmiller98@gmail.com>
2019-05-03 20:15:13 -0600

    Fix gvm nullptr exception.

--- codemp/server/sv_gameapi.cpp
+++ codemp/server/sv_gameapi.cpp
@@ -118,6 +118,8 @@ void GVM_ClientThink( int clientNum, usercmd_t *ucmd ) {
 }
 
 void GVM_RunFrame( int levelTime ) {
+	if (!gvm)
+		return;
 	if ( gvm->isLegacy ) {
 		VM_Call( gvm, GAME_RUN_FRAME, levelTime );
 		return;
