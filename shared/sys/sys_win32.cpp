8acb78983579e007ac1718f027361b783ee7c33b
Bucky <mjmiller98@gmail.com>
2019-12-13 01:13:05 -0700

    Fix client defaulting to onboard graphics on laptops with discrete GPUs.

--- shared/sys/sys_win32.cpp
+++ shared/sys/sys_win32.cpp
@@ -33,6 +33,14 @@ static char homePath[ MAX_OSPATH ] = { 0 };
 
 static UINT timerResolution = 0;
 
+#ifndef DEDICATED
+// Prefer descrete GPU over integrated when both are available.
+// http://developer.download.nvidia.com/devzone/devcenter/gamegraphics/files/OptimusRenderingPolicies.pdf
+extern "C" __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+// https://gpuopen.com/amdpowerxpressrequesthighperformance/
+extern "C" __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+#endif
+
 /*
 ==============
 Sys_Basename
