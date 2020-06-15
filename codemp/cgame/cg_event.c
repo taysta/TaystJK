f75788367c65d6e4372fe0061b3a3014f2b09ee7
Bucky <mjmiller98@gmail.com>
2020-06-15 15:55:56 -0600

    fix vgs console printing twice & w/ blank line

--- codemp/cgame/cg_event.c
+++ codemp/cgame/cg_event.c
@@ -3617,14 +3617,7 @@ void CG_EntityEvent( centity_t *cent, vec3_t position ) {
 				}
 				if (ci->team == ourTeam || isGlobalVGS(s)) //put it to console or.. just not at all?
 				{ //add to the chat box
-					//hear it in the world spot.
-					char vchatstr[1024] = {0};
-					Q_strncpyz(vchatstr, va("<%s^7: %s>\n", ci->name, descr), sizeof( vchatstr ) );
-					CG_ChatBox_AddString(vchatstr);
-					if (cg_chatBox.integer)
-						trap->Print("*%s", vchatstr); //supress in top left w/ the chatbox enabled
-					else
-						trap->Print(vchatstr);
+					CG_ChatBox_AddString(va("<%s^7: %s>", ci->name, descr));
 				}
 
 				if (ci->team == ourTeam || isGlobalVGS(s))
