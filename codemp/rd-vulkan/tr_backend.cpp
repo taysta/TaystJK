5e0466a614530c99605aff054addee9733146f27
JKSunny <jksunny71@hotmail.com>
2022-03-19 11:34:54 +0100

    Vulkan: Fix worldeffects blendmode state bits artifacts e.g. with rain

--- codemp/rd-vulkan/tr_backend.cpp
+++ codemp/rd-vulkan/tr_backend.cpp
@@ -128,9 +128,7 @@ static void RB_Hyperspace(void) {
 	}
 
 	if ( tess.shader != tr.whiteShader ) {
-		if ( tess.numIndexes ) {
-			RB_EndSurface();
-		}
+		RB_EndSurface();
 		RB_BeginSurface( tr.whiteShader, 0 );
 	}
 
@@ -147,6 +145,9 @@ static void RB_Hyperspace(void) {
 		0.0, 0.0, 0.0, 0.0, c );
 
 	RB_EndSurface();
+
+	tess.numIndexes = 0;
+	tess.numVertexes = 0;
 
 	backEnd.isHyperspace = qtrue;
 }
