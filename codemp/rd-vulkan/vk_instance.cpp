3064e5d20ab331f6335683fa3c69be90f2fc708f
taysta <taylor.brough13@gmail.com>
2022-10-06 23:12:50 +0800

    fix vk vid_restart crash under some circumstances on 64-bit linux compilers

--- codemp/rd-vulkan/vk_instance.cpp
+++ codemp/rd-vulkan/vk_instance.cpp
@@ -308,7 +308,7 @@ static void vk_create_instance( void )
 
     result = qvkCreateInstance(&desc, NULL, &vk.instance);
 #endif
-
+    result = qvkCreateInstance(&desc, NULL, &vk.instance);
     switch (result) {
         case VK_SUCCESS:
             vk_debug("--- Vulkan create instance success! ---\n\n"); break;
