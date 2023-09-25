551290f344109134cf66ac99bf42af2f2d83fd01
Razish <mrrazish@gmail.com>
2023-09-25 21:07:27 +0800

    [Shared] Add cl_mouseAspectScaling to correct UI mouse sensitivity for your resolution

--- code/client/client.h
+++ code/client/client.h
@@ -285,6 +285,7 @@ extern	cvar_t	*cl_sensitivity;
 extern	cvar_t	*cl_freelook;
 
 extern	cvar_t	*cl_mouseAccel;
+extern	cvar_t	*cl_mouseAspectScaling;
 extern	cvar_t	*cl_showMouseRate;
 
 extern	cvar_t	*cl_allowAltEnter;
