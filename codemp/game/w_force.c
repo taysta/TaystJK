a67974696e3f189c5ac64660c69e687f993bab77
taysta <taylor.brough13@gmail.com>
2023-10-21 18:17:12 +0800

    [jaPRO/Bucky] Don't let the player 'select' force jump

--- codemp/game/w_force.c
+++ codemp/game/w_force.c
@@ -5329,8 +5329,6 @@ void WP_ForcePowersUpdate( gentity_t *self, usercmd_t *ucmd )
 		return;
 	}
 
-	if (!self->NPC && self->client->ps.fd.forcePowerSelected == FP_LEVITATION)
-		self->client->ps.fd.forcePowerSelected = 0;
 
 	/*
 	if (self->client->ps.fd.saberAnimLevel > self->client->ps.fd.forcePowerLevel[FP_SABER_OFFENSE])
