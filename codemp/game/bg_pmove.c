ddee6893972f922f008c1e14a45c04324577ee75
videoP <videoprofess@gmail.com>
2023-12-30 21:53:40 -0500

    dodge tweak
    (cherry picked from commit 291f5eefa1f39a05b157de1402e0dffefa16e6cb)

--- codemp/game/bg_pmove.c
+++ codemp/game/bg_pmove.c
@@ -3975,14 +3975,18 @@ static void PM_DodgeMove(int forward, int right)
 	int DODGE_SPEED = pm->ps->speed * 1.25f;
 	int DODGE_JUMP_SPEED = 180;
 
-	if (pm->ps->stats[STAT_MOVEMENTSTYLE] == MV_TRIBES) {
-		DODGE_SPEED = pm->ps->speed * 1.5f;
-		DODGE_JUMP_SPEED = 0;
-	}
-
 	VectorMA( vec3_origin, right, pml.right, dodgedir );
 	VectorMA( dodgedir, forward, pml.forward, dodgedir );
 	VectorNormalize( dodgedir );
+
+	if (pm->ps->stats[STAT_MOVEMENTSTYLE] == MV_TRIBES) {
+		DODGE_SPEED = pm->ps->speed * 1.5f;
+		DODGE_JUMP_SPEED = 0;
+#ifdef _GAME
+		G_PlayEffect(EFFECT_LANDING_SAND, pm->ps->origin, dodgedir);//Should be spot on wall, and wallnormal, a better, predicted way to do this?
+#endif
+	}
+
 	VectorScale( dodgedir, DODGE_SPEED, dodgedir );
 
 	VectorCopy( dodgedir, pm->ps->velocity );
@@ -4031,8 +4035,9 @@ static void PM_CheckDash(void)
 	if (pm->ps->weaponTime > 0)
 		return;
 
-	if (moveStyle == MV_WSW || moveStyle == MV_TRIBES) {
-
+	if (moveStyle == MV_WSW) {
+	}
+	else if (moveStyle == MV_TRIBES && (pm->ps->velocity[0]* pm->ps->velocity[1]) > pm->ps->speed * 1.5f) {
 	}
 	else return;
 
