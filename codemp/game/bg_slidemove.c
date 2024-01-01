e6b75d468cf77fe2f348ae705097e8a947d8ba02
videoP <videoprofess@gmail.com>
2024-01-02 07:28:20 +0800

    splashdmg flag,  edgebug fix option (test)
    (cherry picked from commit 9fad9756ac028f731e130f7d06749a51225d5179)

--- codemp/game/bg_slidemove.c
+++ codemp/game/bg_slidemove.c
@@ -820,8 +820,16 @@ qboolean	PM_SlideMove( qboolean gravity ) {
 
 
 			// slide along the plane
-			PM_ClipVelocity (pm->ps->velocity, planes[i], clipVelocity, OVERCLIP );
-
+			if (pm->ps->stats[STAT_RACEMODE] && pm->ps->stats[STAT_MOVEMENTSTYLE] == MV_OCPM) {
+				float offset = OVERCLIP;
+				if (bot_strafeOffset.value) {
+					offset = bot_strafeOffset.value;
+					Com_Printf("Overwriting overclip!\n");
+				}
+				PM_ClipVelocity(pm->ps->velocity, planes[i], clipVelocity, bot_strafeOffset.value); //Loda this is causing deadstops on vertical seams.  1.5f?
+			}
+			else 
+				PM_ClipVelocity(pm->ps->velocity, planes[i], clipVelocity, OVERCLIP);
 			// slide along the plane
 			PM_ClipVelocity (endVelocity, planes[i], endClipVelocity, OVERCLIP );
 
