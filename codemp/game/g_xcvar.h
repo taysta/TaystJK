9ea03182b135afa7bf7f29f0f500aa5dc39b2dca
videoP <videoprofess@gmail.com>
2024-01-03 14:56:33 +0800

    checkpoint and neutral flag timer tweak
    (cherry picked from commit 40108ed50784dfdf7d689d3c063c0b67bbcf92b2)

--- codemp/game/g_xcvar.h
+++ codemp/game/g_xcvar.h
@@ -185,6 +185,7 @@ XCVAR_DEF( g_fixCTFScores,				"0",			NULL,				CVAR_ARCHIVE,									qtrue )
 XCVAR_DEF( g_fixFlagHitbox,				"0",			NULL,				CVAR_ARCHIVE,									qtrue )
 XCVAR_DEF( g_fixExplosiveHitboxes,		"1",			NULL,				CVAR_ARCHIVE,									qtrue ) //fixes hitboxes on placed explosives (tripmines/detpacks)
 XCVAR_DEF( g_neutralFlag,				"0",			CVU_Rabbit,			CVAR_ARCHIVE,									qtrue )
+XCVAR_DEF( g_neutralFlagTimer,			"10000",		NULL,				CVAR_ARCHIVE,									qtrue)
 
 //JAPRO Saber
 XCVAR_DEF( g_tweakSaber,				"0",			CVU_TweakSaber,		CVAR_ARCHIVE,									qtrue )
