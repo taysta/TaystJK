df2496ebbc1a9a5a4c1e67d31d4c335353d0df7b
videoP <videoprofess@gmail.com>
2024-01-21 01:53:27 -0500

    proj sniper no drop
    (cherry picked from commit eefd38595d1788730aed61432764b657d26cc268)

--- codemp/game/bg_pmove.c
+++ codemp/game/bg_pmove.c
@@ -4391,6 +4391,7 @@ static void PM_GrappleMoveTribes(void) {
 
 		if (wishSpeed < 300)
 			wishSpeed = 300;
+		//else give this a max so we cant tether to people going like 300kph effectively?
 
 		//oldVel = VectorLength(pm->ps->velocity);
 		PM_Accelerate(diffNormal, wishSpeed, pullStrength); //600 is WishSpeed
