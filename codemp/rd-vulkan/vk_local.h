82ffb3c87b6f3d6abebc3e4887759c3c671e29d9
JKSunny <60109631+JKSunny@users.noreply.github.com>
2024-02-11 14:45:55 +0100

    Vulkan: Add debug utills with define guards

--- codemp/rd-vulkan/vk_local.h
+++ codemp/rd-vulkan/vk_local.h
@@ -52,6 +52,8 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 #if defined (_DEBUG)
 #if defined (_WIN32)
 #define USE_VK_VALIDATION
+#define USE_DEBUG_REPORT
+//#define USE_DEBUG_UTILS
 #endif
 #endif
 
@@ -292,8 +294,14 @@ extern PFN_vkGetPhysicalDeviceSurfaceFormatsKHR		    qvkGetPhysicalDeviceSurface
 extern PFN_vkGetPhysicalDeviceSurfacePresentModesKHR	qvkGetPhysicalDeviceSurfacePresentModesKHR;
 extern PFN_vkGetPhysicalDeviceSurfaceSupportKHR		    qvkGetPhysicalDeviceSurfaceSupportKHR;
 #ifdef USE_VK_VALIDATION
-extern PFN_vkCreateDebugReportCallbackEXT				qvkCreateDebugReportCallbackEXT;
-extern PFN_vkDestroyDebugReportCallbackEXT				qvkDestroyDebugReportCallbackEXT;
+	#ifdef USE_DEBUG_REPORT
+		extern PFN_vkCreateDebugReportCallbackEXT				qvkCreateDebugReportCallbackEXT;
+		extern PFN_vkDestroyDebugReportCallbackEXT				qvkDestroyDebugReportCallbackEXT;
+	#endif
+	#ifdef USE_DEBUG_UTILS
+		extern PFN_vkCreateDebugUtilsMessengerEXT				qvkCreateDebugUtilsMessengerEXT;
+		extern PFN_vkDestroyDebugUtilsMessengerEXT				qvkDestroyDebugUtilsMessengerEXT;
+	#endif
 #endif
 extern PFN_vkAllocateCommandBuffers					    qvkAllocateCommandBuffers;
 extern PFN_vkAllocateDescriptorSets					    qvkAllocateDescriptorSets;
@@ -585,7 +593,12 @@ typedef struct {
 	char			instance_extensions_string[MAX_STRING_CHARS];
 
 #ifdef USE_VK_VALIDATION
-	VkDebugReportCallbackEXT debug_callback;
+	#ifdef USE_DEBUG_REPORT
+		VkDebugReportCallbackEXT debug_callback;
+	#endif
+	#ifdef USE_DEBUG_UTILS
+		VkDebugUtilsMessengerEXT debug_utils_messenger;
+	#endif
 #endif
 
 	uint32_t		queue_family_index;
@@ -1052,5 +1065,12 @@ void		vk_set_object_name( uint64_t obj, const char *objName, VkDebugReportObject
 }
 
 void		vk_debug( const char *msg, ... );
-void		vk_create_debug_callback( void );
 void		R_DebugGraphics( void );
+
+#ifdef USE_VK_VALIDATION
+	void	vk_create_debug_callback( void );
+
+#ifdef USE_DEBUG_UTILS
+	void	vk_create_debug_utils( VkDebugUtilsMessengerCreateInfoEXT &desc );
+#endif
+#endif
