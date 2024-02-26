ca7f6d2120323d03a48b453756474716868dfb86
Razish <mrrazish@gmail.com>
2024-02-26 23:42:43 +0800

    add con_timestamps 2 to disable timestamps in the notify lines only

--- codemp/client/cl_console.cpp
+++ codemp/client/cl_console.cpp
@@ -896,6 +896,7 @@ Draws the last few lines of output transparently over the game top
 void Con_DrawNotify (void)
 {
 	int		x, v;
+	int		lineLimit = con.linewidth;
 	conChar_t		*text;
 	int		i;
 	int		time;
@@ -929,9 +930,10 @@ void Con_DrawNotify (void)
 		if (time > con_notifytime->value*1000)
 			continue;
 		text = con.text + (i % con.totallines)*con.rowwidth;
-
-		if (con_timestamps->integer != 1)
+		if (con_timestamps->integer == 0 || con_timestamps->integer == 2) {
 			text += CON_TIMESTAMP_LEN;
+			lineLimit -= CON_TIMESTAMP_LEN;
+		}
 
 		if (cl.snap.ps.pm_type != PM_INTERMISSION && Key_GetCatcher( ) & (KEYCATCH_UI | KEYCATCH_CGAME) ) {
 			continue;
@@ -953,7 +955,7 @@ void Con_DrawNotify (void)
 			//
 			char sTemp[4096];	// ott
 			sTemp[0] = '\0';
-			for (x = 0 ; x < con.linewidth ; x++)
+			for (x = 0 ; x < lineLimit ; x++)
 			{
 				if ( text[x].f.color != currentColor ) {
 					currentColor = text[x].f.color;
@@ -971,8 +973,8 @@ void Con_DrawNotify (void)
 		}
 		else
 		{
-			for (x = 0 ; x < con.linewidth ; x++) {
-				if ( text[x].f.character == CON_BLANK_CHAR ) {
+			for (x = 0 ; x < lineLimit ; x++) {
+				if ( text[x].f.character == ' ' ) {
 					continue;
 				}
 				if ( text[x].f.color != currentColor ) {
