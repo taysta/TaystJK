8eb84daec5067810e14c8e2c3c5c0bae1e328ab6
videoP <videoprofess@gmail.com>
2024-05-15 00:13:27 -0400

    proper one flag CTF with g_neutralflag 6 in ctf
    (cherry picked from commit 34fa19f822c637fef44d48730391e77efc419976)

--- codemp/game/g_cvar.c
+++ codemp/game/g_cvar.c
@@ -247,7 +247,7 @@ static void CVU_Grapple(void) {
 }
 */
 
-static void RemoveRabbit(void) {
+void RemoveRabbit(void) {
 	int i;
 	gclient_t	*cl;
 	gentity_t	*ent;
@@ -470,7 +470,7 @@ void CVU_Rabbit( void ) {
 			if (!G_CallSpawn(ent))
 				G_FreeEntity( ent );
 		}
-		else if (((g_neutralFlag.integer == 4 || g_neutralFlag.integer == 5) && (level.gametype == GT_CTF))) { //One Flag CTF
+		else if (((g_neutralFlag.integer == 4 || g_neutralFlag.integer == 5 || g_neutralFlag.integer == 6) && (level.gametype == GT_CTF))) { //One Flag CTF
 			//Remove and spawn neutral flag.  Remove CTF flags
 
 			RemoveRabbit(); //Delete the current flag first
@@ -483,7 +483,7 @@ void CVU_Rabbit( void ) {
 			if (!G_CallSpawn(ent))
 				G_FreeEntity(ent);
 		}
-		else if (((g_neutralFlag.integer != 4 && g_neutralFlag.integer != 5) && (level.gametype == GT_CTF))) { //turned off neutral flag in CTF gametype
+		else if (((g_neutralFlag.integer != 4 && g_neutralFlag.integer != 5 && g_neutralFlag.integer != 6) && (level.gametype == GT_CTF))) { //turned off neutral flag in standard CTF gametype
 			//Remove neutral flag.  Remove and add CTF flags.
 			RemoveRabbit(); //Delete the current flag first
 			RemoveRedBlueFlags();
