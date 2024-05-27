3399d12dde2770d3356feab2fadcdd4c87323a32
videoP <videoprofess@gmail.com>
2024-05-26 21:48:03 -0400

    opsi
    (cherry picked from commit c71db031ac8553a6056974521eac0a397b904af0)

--- codemp/game/g_weapon.c
+++ codemp/game/g_weapon.c
@@ -257,7 +257,7 @@ BRYAR PISTOL
 */
 
 //---------------------------------------------------------
-static void WP_FireColt(gentity_t *ent)
+static void WP_FireColt(gentity_t *ent, qboolean altFire)
 //---------------------------------------------------------
 {
 	int			damage = 40;
