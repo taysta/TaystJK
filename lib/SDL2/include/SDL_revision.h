7f229473fbba8118f819ed8309528f12a2ed5121
taysta <taylor.brough13@gmail.com>
2025-04-26 18:25:47 +0800

    [SDL2] Changes from 8f56a46442f1fc461d1371b8b7a96a352eb14f55 by Ensiform

--- lib/SDL2/include/SDL_revision.h
+++ lib/SDL2/include/SDL_revision.h
@@ -1,8 +1,6 @@
-/* #undef SDL_VENDOR_INFO */
-#define SDL_REVISION_NUMBER 0
-
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-release-2.32.4-0-g2359383fc (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION SDL_VENDOR_INFO
 #else
-#define SDL_REVISION "SDL-release-2.32.4-0-g2359383fc"
+#define SDL_REVISION ""
 #endif
+#define SDL_REVISION_NUMBER 0
