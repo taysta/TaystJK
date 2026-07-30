4ab74b6f9d702047e2f9e6ac43fb0c273ebbae85
tayst <taylor.brough13@gmail.com>
2026-07-30 19:10:04 +0800

    [client] Apply fix from https://github.com/JACoders/OpenJK/pull/1334

--- codemp/client/snd_dma.cpp
+++ codemp/client/snd_dma.cpp
@@ -4136,8 +4136,12 @@ static qboolean S_StartBackgroundTrack_Actual( MusicInfo_t *pMusicInfo, qboolean
 	char	dump[16];
 	char	name[MAX_QPATH];
 
-	Q_strncpyz( sMusic_BackgroundLoop, loop, sizeof( sMusic_BackgroundLoop ));
+	char	*bgLoop = sMusic_BackgroundLoop;
 
+	if ( bgLoop != loop )
+	{// don't copy into yourself
+		Q_strncpyz( sMusic_BackgroundLoop, loop, sizeof( sMusic_BackgroundLoop ));
+	}
 	Q_strncpyz( name, intro, sizeof( name ) - 4 );	// this seems to be so that if the filename hasn't got an extension
 													//	but doesn't have the room to append on either then you'll just
 													//	get the "soft" fopen() error, rather than the ERR_DROP you'd get
